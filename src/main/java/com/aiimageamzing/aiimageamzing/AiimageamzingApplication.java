749682fd61f47c9f75881c9c122e752309500544
bumjun2 <jbj3713@naver.com>
2024-01-15 13:46:25 +0900

    # 초기 설정

---
+++ src/main/java/com/aiimageamzing/aiimageamzing/AiimageamzingApplication.java
@@ -0,0 +1,13 @@
+package com.aiimageamzing.aiimageamzing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiimageamzingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiimageamzingApplication.class, args);
+    }
+
+}
